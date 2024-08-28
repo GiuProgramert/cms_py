81f2cf5d1f0e66b85ac2a3d2a09a3e894b9261c4
Andres Roman <androceomimo@fpuna.edu.py>
2024-08-28 09:07:30 -0400

    Resolved conflict by keeping the modified version of login.html

--- cms_py/urls.py
+++ cms_py/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from user.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('login/', login_view, name='login'),
+    path('', lambda request: redirect('login')),
 ]
